54562c01b11fff904d822144a3a1d863f902979f
Ignacio Seco <ignacio.seco@inacapmail.cl>
2021-11-02 16:08:22 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/pages/AsignaturaPage.js
+++ src/pages/AsignaturaPage.js
@@ -41,6 +41,8 @@ export const AsignaturaPage = () => {
     const tareas = useSelector(state => state.unidades.tareas);
     localStorage.setItem('idAsignatura',idAsignatura);
 
+    let tAlumno = {}
+
     return (
         <div>
             <div className="d-flex flex-row align-items-baseline ">
@@ -53,8 +55,11 @@ export const AsignaturaPage = () => {
                         const filterMateriales = materiales.filter(material => material.idUnidad === unidad._id)
                         const filterPruebas = pruebas.filter(prueba => prueba.idUnidad === unidad._id)
                         const filterJob = tareas?.filter(tarea => tarea.idUnidad === unidad._id && tarea.enunciados.length !== 0)
+                        filterJob.map( t => {
+                            tAlumno = tareaAlumno.filter( tAlumno => tAlumno.idTarea === t.id && tAlumno.idUsuario === _id && tAlumno.idUnidad === t.idUnidad )
+                        })
                         return(
-                       <Unidad key={unidad._id} unidad={unidad} index={index+1} tareas={filterJob} materiales={filterMateriales} pruebas={filterPruebas} tareaAlumno={ tareaAlumno } /> 
+                       <Unidad key={unidad._id} unidad={unidad} index={index+1} tareas={filterJob} materiales={filterMateriales} tareaAlumno={ tAlumno } /> 
                     )})
                 }
             </div>
